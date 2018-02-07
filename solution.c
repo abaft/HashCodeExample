046b1bbcc67b765f76b92a7c5ea253cece83cdd5
Benjamin Chalmers <benj@minchalme.rs>
2018-02-07 19:40:45 +0000

    Sort of works

--- solution.c
+++ solution.c
@@ -4,9 +4,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+//#include "pcg-c/include/pcg_variants.h"
 
-#define DEPTH 10000
-int recorder;
+#define DEPTH 1000000
 
 typedef struct _pizza
 {
@@ -44,7 +44,8 @@ pizza parseFile(char* fileName)
   rtn.C = values[1];
   rtn.L = values[2];
   rtn.H = values[3];
-  rtn.pizzaData = malloc (sizeof(char) * rtn.R * rtn.C * 10);
+  printf("%i %i %i %i", rtn.R, rtn.C, rtn.L, rtn.H);
+  rtn.pizzaData = malloc (sizeof(char) * rtn.R * rtn.C * 2 );
 
   char* line = rtn.pizzaData;
   size_t n = sizeof(char) * rtn.C;
@@ -61,7 +62,7 @@ pizza parseFile(char* fileName)
 
 char* navC(pizza p, int r, int c) { return (p.pizzaData + c + r * p.C); }
 
-char checkSlice (pizza *p, slice s)
+char checkSlice (pizza p, slice s, pizza nullMask)
 {
   int m = 0;
   int t = 0;
@@ -69,15 +70,18 @@ char checkSlice (pizza *p, slice s)
   for (int i = 0; i <= abs(s.r2-s.r1); ++i)
     for (int j = 0; j <= abs(s.c2-s.c1); ++j)
     {
-      if (*navC(*p, s.r1+i, s.c1+j) == 'M')
-        ++m;
-      else if (*navC(*p, s.r1+i, s.c1+j) == 'T')
-        ++t;
-      else if (*navC(*p, s.r1+i, s.c1+j) == 'X')
+      if (*navC(nullMask, s.r1+i, s.c1+j) != 'X')
+      {
+        if (*navC(p, s.r1+i, s.c1+j) == 'M')
+          ++m;
+        else if (*navC(p, s.r1+i, s.c1+j) == 'T')
+          ++t;
+      }
+      else
         return 0;
     }
 
-  return m >= p->L && t >= p->L && (m + t) <= p->H;
+  return m >= p.L && t >= p.L && (m + t) <= p.H;
 }
 
 char nullSlice (pizza *p, slice s)
@@ -109,38 +113,57 @@ int sliceArrayScore (slice* s, int n)
 {
   int score = 0;
   for (int i = 0; i < n; ++i)
-    score += ((s[i].r2-s[i].r1) + 1) + ((s[i].c2-s[i].r1) + 1);
+    score = score + (((s[i].r2-s[i].r1) + 1) * ((s[i].c2-s[i].c1) + 1));
+  return score;
 }
 
-slice* sliceArrayGen (pizza p)
+slice* sliceArrayGen (pizza p, int* n)
 {
   slice* 
     rtn = malloc(sizeof(slice) * p.R * p.C); //Make it big enough for anything
-  int n = 0;
+  pizza nullMask = p;
+  
+  nullMask.pizzaData = malloc (p.R * p.C * 20 * sizeof(char));
+  for (long long i = 0; i < (p.R * p.C * 2 * sizeof(char)); ++i)
+    nullMask.pizzaData[i] = 'O';
+  *n = 0;
   for (int i = 0; i < DEPTH; ++i)
   {
     slice tmp;
-    if (checkSlice(&p, (tmp = randomSlicer(p))))
+    if (checkSlice(p, (tmp = randomSlicer(p)), nullMask))
     {
-      nullSlice (&p, tmp);
-      printf("%i, %i, %i, %i\n", tmp.r1, tmp.c1, tmp.r2, tmp.c2);
-      rtn[n++] = tmp; 
+      nullSlice (&nullMask, tmp);
+  //    printf ("%i, %i, %i, %i\n", tmp.r1, tmp.c1, tmp.r2, tmp.c2);
+      rtn[*n] = tmp; 
+      *n = *n + 1;
     }
   }
-  recorder = n;
+
+  free(nullMask.pizzaData);
   return rtn;
 }
 
 int main(int argc, char** argv)
 {
-  pizza p = parseFile("example.in");
+
+//  pcg32_srandom(42u, 54u);
+
+  int max = 0;
+  int n;
+  pizza p = parseFile(argv[1]);
   slice* s;
+  srand(time(NULL));
   do
   {
-    srand(time(NULL));
-    s = sliceArrayGen(p);
-    p = parseFile("example.in");
-    printf("%i\n", sliceArrayScore(s, recorder));
+    s = sliceArrayGen(p, &n);
+    int score = sliceArrayScore(s, n);
+    if (score > max)
+    {
+      max = score;
+      printf("%i %i\n", score, n);
+      for (int i = 0; i < n; ++i)
+      printf ("%i, %i, %i, %i\n", s[i].r1, s[i].c1, s[i].r2, s[i].c2);
+    }
     free(s);
   }while(1);
 }
